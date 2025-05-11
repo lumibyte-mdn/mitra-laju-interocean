8484c9962f4963754e5b98bc2051d61759884be5
winstencoellins <winstencoellins13@gmail.com>
2025-05-11 17:47:22 +0700

    add: clerk credential

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.push("/dashboard")
+  }, [])
+
   return (
     <>
       <div className="text-black font-bold">
-        Home
+        
       </div>
     </>
   );
